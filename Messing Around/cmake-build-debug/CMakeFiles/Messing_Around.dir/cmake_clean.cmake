a4af792ab160e755838ca119ce6cfa626c16c376
Unknown <davekerk7@gmail.com>
2017-01-24 08:58:27 -0700

    Added Web Storm Code

---
+++ Messing Around/cmake-build-debug/CMakeFiles/Messing_Around.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Messing_Around.dir/main.cpp.obj"
+  "Messing_Around.pdb"
+  "Messing_Around.exe"
+  "Messing_Around.exe.manifest"
+  "libMessing_Around.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Messing_Around.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Messing Around/cmake-build-debug/CMakeFiles/Messing_Around.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Messing_Around.
+# This may be replaced when dependencies are built.
