b69522603091352b00ede8b5af2fbb39b8fb0a52
Unknown <davekerk7@gmail.com>
2017-02-13 14:24:48 -0700

    Added Chess Board

---
+++ Chess Board/cmake-build-debug/CMakeFiles/Chess_Board.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess_Board.dir/main.cpp.obj"
+  "Chess_Board.pdb"
+  "Chess_Board.exe"
+  "Chess_Board.exe.manifest"
+  "libChess_Board.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chess_Board.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ More CPP Practice/cmake-build-debug/CMakeFiles/More_CPP_Practice.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/More_CPP_Practice.dir/main.cpp.obj"
+  "More_CPP_Practice.pdb"
+  "More_CPP_Practice.exe"
+  "More_CPP_Practice.exe.manifest"
+  "libMore_CPP_Practice.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/More_CPP_Practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
