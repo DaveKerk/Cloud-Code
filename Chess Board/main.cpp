edff4e32388b5917da6922ac5ea95079ef60aaa9
Unknown <davekerk7@gmail.com>
2017-02-13 14:50:39 -0700

    Alignment

--- Chess Board/main.cpp
+++ Chess Board/main.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
 
-int main()
-{
-    for (int j=0; j < 4; j++)
-    {
-        for (int x = 0;x<3 ;x++) //(x==1||x==3||x==5||x==7))
+int main() {
+    for (int j = 0; j < 4; j++) {
+        for (int x = 0; x < 3; x++) //(x==1||x==3||x==5||x==7))
         {
-            for (int m = 0; m < 4; m++)
-            {
+            for (int m = 0; m < 4; m++) {
                 cout << setw(10) << "#####";
             }
             cout << endl;
         }
-        for (int l=0; l < 3; l++)
-        {
+        for (int l = 0; l < 3; l++) {
             cout << "#####";
-            for(int n=0;n<3 ;n++) //(n==2||n==4||n==6||n==8))
+            for (int n = 0; n < 3; n++) //(n==2||n==4||n==6||n==8))
             {
                 cout << setw(10) << "#####";
             }
             cout << endl;
         }
     }
+    //cout << setw(50) << "This is a chess board" << left;
     return 0;
 }
