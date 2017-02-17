41d30ae3af9edf4bf828d859c57dfcc3da588d09
Unknown <davekerk7@gmail.com>
2017-02-17 13:31:16 -0700

    Messing

--- Angular Chess/app.js
+++ Angular Chess/app.js
@@ -1,14 +1,21 @@
 var angular;
 var app = angular.module('app', []);
+
 app.controller('appctrl', ['$scope', function ($scope) {
-        $scope.asdf = 99;
-        function messenger() {
-            console.log('here');
-            $scope.asdf++;
-        }
-        app.controller('appctrl', ['$scope', function ($scope) {
-                $scope.scope = 0;
-                $scope.oneError = messenger;
-            }]);
-    }]);
+	$scope.asdf = 99;
+	function messenger() {
+		console.log('here');
+		$scope.asdf++;
+	}
+
+	app.controller('appctrl', ['$scope', function ($scope) {
+		$scope.scope = 0;
+		$scope.oneError = messenger;
+	}]);
+}]);
+
+app.controller('myController', ['$scope', function ($scope) {
+	$scope.value = 72;
+}]);
+
 //# sourceMappingURL=app.js.map
