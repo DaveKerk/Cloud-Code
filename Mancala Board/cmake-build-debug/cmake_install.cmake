426deed9212673865001850a88f99b2edf08e46c
Unknown <davekerk7@gmail.com>
2017-03-06 13:32:48 -0700

    Input Numbers
    Added functions to input hold amounts.

--- Mancala Board/cmake-build-debug/cmake_install.cmake
+++ Mancala Board/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/DaveKerk/Desktop/Cloud-Code/Mancala Board
+# Install script for directory: C:/Users/david/Desktop/Cloud Code/Mancala Board
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Mancala_Board")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Mancala_Board_")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/DaveKerk/Desktop/Cloud-Code/Mancala Board/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/david/Desktop/Cloud Code/Mancala Board/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Mancala Board/main.cpp
+++ Mancala Board/main.cpp
@@ -22,47 +22,47 @@ int initialRocks() {
     // rocks[10] = 4;
     // rocks[11] = 4;
     // rocks[12] = 4;
-    rocks[0] = 0;
-    rocks[1] = 1;
-    rocks[2] = 2;
-    rocks[3] = 3;
-    rocks[4] = 4;
-    rocks[5] = 5;
-    rocks[6] = 6;
-    rocks[7] = 7;
-    rocks[8] = 8;
-    rocks[9] = 9;
-    rocks[10] = 10;
-    rocks[11] = 11;
-    rocks[12] = 12;
-    rocks[13] = 13;
+    // rocks[0] = 0;
+    // rocks[1] = 1;
+    // rocks[2] = 2;
+    // rocks[3] = 3;
+    // rocks[4] = 4;
+    // rocks[5] = 5;
+    // rocks[6] = 6;
+    // rocks[7] = 7;
+    // rocks[8] = 8;
+    // rocks[9] = 9;
+    // rocks[10] = 10;
+    // rocks[11] = 11;
+    // rocks[12] = 12;
+    // rocks[13] = 13;
 }
 
-// Top and Bottom
+// Top and bottom.
 int allStars() {
     for (int i = 0; i <= 57; i++)cout << '*';
 }
 
-// Straight Line of Stars on Middle Line
+// Straight line of stars on middle line.
 int lessAllStars() {
     for (int j = 0; j <= 42; j++)cout << '*';
 }
 
-// The Middle Line
+// The middle line.
 int middle() {
     cout << "*  13  ";
     lessAllStars();
     cout << "   6   *";
 }
 
-// Normal Line Without Numbers
+// Normal line without numbers.
 int normStars() {
     cout << '*';
     for (int e = 0; e <= 6; e++)cout << setw(7) << '*';
     cout << setw(8) << '*';
 }
 
-// Numbers 0 Through 5
+// Numbers 0 through 5.
 int upNum() {
     cout << '*' << " ";
     for (int k = 0; k <= 7; k++) {
@@ -76,7 +76,7 @@ int upNum() {
     }
 }
 
-// Numbers 7 Through 12
+// Numbers 7 through 12.
 int downNum() {
     cout << '*';
     for (int l = 13; l >= 6; l--) {
@@ -125,6 +125,7 @@ int scoreDisplayMiddle() {
     cout << setw(4) << rocks[6] << setw(4) << '*';
 }
 
+//Outputs the entire board.
 int entireBoard() {
     for (int w = 0; w <= 13; w++) {
         if (w == 1 || w == 13) {
@@ -148,6 +149,7 @@ int entireBoard() {
     }
 }
 
+//Controls attempt.
 int controls() {
     int holdNumber;
     cin >> holdNumber;
@@ -159,10 +161,40 @@ int controls() {
     }
 }
 
+//Function to number the top holds.
+int blockNumbererUp() {
+    for (int i = 0; i <= 5; i++) {
+        cout << "Enter the value of block " << i << ":";
+        cin >> rocks[i];
+        if (rocks[i] < 0) {
+            rocks[i] *= -1;
+        }
+        if (rocks[i] > 100) {
+            rocks[i] %= 100;
+        }
+    }
+}
+
+//Function to number the bottom holds.
+int blockNumbererDown() {
+    for (int i = 7; i <= 12; i++) {
+        cout << "Enter the value of block " << i << ":";
+        cin >> rocks[i];
+        if (rocks[i] < 0) {
+            rocks[i] *= -1;
+        }
+        if (rocks[i] > 100) {
+            rocks[i] %= 100;
+        }
+    }
+}
+
+
 int main() {
     initialRocks();
     entireBoard();
-    controls();
+    blockNumbererUp();
+    blockNumbererDown();
     entireBoard();
     return 0;
 }
