07eb0612fb561b735cd69776c7b5cb6824b10fde
Unknown <davekerk7@gmail.com>
2017-04-18 17:59:39 -0600

    Array Maker

--- Heap Sort/main.cpp
+++ Heap Sort/main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-#include <time.h>
+#include <iomanip>
 
 using namespace std;
 
+void heapify(){
+
+}
 
 int main() {
     srand(time(NULL));
-    cout << (rand() % 100) + 1;
+    cout << "How many item would you like in your array?\n"
+         << "100?\n"
+         << "1000?\n"
+         << "10000?\n";
+
+    //The following code generates an array of *arrayCount* random numbers using
+    //srand to randomize the numbers as they are generated and placed in the array.
+    int arrayCount;
+    cin >> arrayCount;
+    int mainArray[arrayCount];
+    for (int i = 0; i < arrayCount; i++)
+        mainArray[i] = ((rand() % arrayCount) + 1);
+
+
+
 
     return 0;
 }
